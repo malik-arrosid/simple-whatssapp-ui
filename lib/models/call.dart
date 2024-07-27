bb464f9fbb962396ce0f5765860e3034e47acc66
malik-arrosid <malikarrosid.md@gmail.com>
2024-07-27 18:44:44 +0900

    first commit

---
+++ lib/models/call.dart
@@ -0,0 +1,12 @@
+class Call {
+  String name;
+  String statusDate;
+
+  Call({required this.name, required this.statusDate});
+}
+
+var callList = [
+  Call(name: "Rosa", statusDate: "11 March, 12:17 pm"),
+  Call(name: "Budi", statusDate: "13 February, 8:11 am"),
+  Call(name: "Johan", statusDate: "12/19/23, 7:39 am"),
+];
---
+++ lib/models/status.dart
@@ -0,0 +1,12 @@
+class Status {
+  String name;
+  String statusDate;
+
+  Status({required this.name, required this.statusDate});
+}
+
+var statusList = [
+  Status(name: "Johan", statusDate: "8 minutes ago"),
+  Status(name: "Rosa", statusDate: "8:15 am"),
+  Status(name: "Budi", statusDate: "Yesterday"),
+];
